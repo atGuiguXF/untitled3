23195b24d9310fc97a7e709b2ac9d7b3d42dfe93
xufeng-19990209 <1344544155@qq.com>
2022-08-19 10:10:23 +0800

    bbb

--- mybatis/src/main/java/com/heying/mybatis/Test.java
+++ mybatis/src/main/java/com/heying/mybatis/Test.java
@@ -1,2 +1,22 @@
-package com.heying.mybatis;public class Test {
+package com.heying.mybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().
+                build(inputStream);
+        //一级缓存
+        SqlSession session = sqlSessionFactory.openSession();
+
+    }
 }
