cc3a7db6c5c57e83ee9260bbe2e749fa5c17662c
xufeng-19990209 <1344544155@qq.com>
2022-08-26 20:17:49 +0800

    aaa

--- springboot-taco/src/main/java/com/heying/springboottaco/controller/DesignTacoController.java
+++ springboot-taco/src/main/java/com/heying/springboottaco/controller/DesignTacoController.java
@@ -38,7 +38,7 @@ public class DesignTacoController {
         }
 
         final Taco taco = new Taco();
-        taco.setName("这是默认 Taco 的签名");
+        taco.setName("这是默认 Taco 的签名");//果然厉害
         model.addAttribute("design", taco);
         return "design";
     }
