2bf32d0afe5df95847efb9f3c6df2801949bd086
xufeng-19990209 <1344544155@qq.com>
2022-08-27 11:40:14 +0800

    aaa

--- springboot-taco/src/main/java/com/heying/springboottaco/controller/HomeController.java
+++ springboot-taco/src/main/java/com/heying/springboottaco/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping("/")//一级路径
     @ResponseBody
     public String home() {
 
